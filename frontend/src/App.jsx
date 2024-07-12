0721f44e849410c5512af4eda0889bf2bb6ce49a
anavarro81 <87661508+anavarro81@users.noreply.github.com>
2024-07-12 18:20:49 +0200

    home-screen (#3)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import Login from './Pages/Login/Login'
+import Home from './Pages/Home/Home'
 
 function App() {
 
 
   return (
-    <>
-    <Login/>    
-    </>
+    <div className='main'>
+    {/* <Login/> */}
+    <Home/>
+    
+    </div>
   )
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,7 +8,13 @@ export default {
     extend: {
       colors: {
         greyDesign: "#D9D9D9",
-      }
+        primary: "#242054",
+        lightGrey: "#E4E7F3"
+
+      },
+      // fontFamily: {
+      //   roboto: ['Roboto', 'sans-serif'],
+      // },
     },
   },
   plugins: [],
