57484116e52a49fee7519b0db9a961992b48dca5
anavarro81 <87661508+anavarro81@users.noreply.github.com>
2024-07-17 17:29:51 +0200

    Dev (#14)
    * primera version de login (#1)
    
    * primera version de login (#2)
    
    * home-screen (#3)
    
    * Home screen (#4)
    
    * home-screen
    
    * fix posicionar el footer en la parte inferior de la pantalla
    
    * corregir posicion footer
    
    * se sube el header en un archivo indepdendiente (#5)
    
    * Tranferencias1 (#6)
    
    * se sube el header en un archivo indepdendiente
    
    * configuracion de colores tailwind y iconos centralizados
    
    * Registro version 1
    
    * diseño de pantallas de trasferencias (#8)
    
    * registro listo
    
    * 9 login validation (#10)
    
    * ft login validations and data
    
    * cambiar los password por otro validos
    
    * ft/login and data
    
    * ft/error-page (#12)
    
    * se creo la ruta de pagar un servicio y se agregaron algunos link
    
    * flujo de pago de servicio, casi listo
    
    ---------
    
    Co-authored-by: GloriaRamosM <gloriaramosmoran@gmail.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Login from "./pages/Login/Login.jsx"
+import Register from "./pages/register/Register.jsx"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Register2 from './pages/register/Register2.jsx';
+import Register3 from './pages/register/Register3.jsx';
+import Home from './pages/Home/Home.jsx'
+import Transfer from './pages/Transfer/Transfer'
+// import Footer from './components/Footer'
+// import Success from './Pages/Transfer/Success.jsx';
+import TransferStepContainer from './pages/Transfer/TransferStepContainer.jsx';
+import ErrorPage from './pages/ErrorPage/ErrorPage.jsx';
+
+import './css/main.css'
+import Pay from './pages/Servicespay/Pay.jsx';
+import PayStepContainer from './pages/Servicespay/PayStepContainer.jsx';
+
+
 
 function App() {
 
 
   return (
+
     <>
-      <h1 className="text-3xl font-bold underline text-red-400">
-        Hello world!!!
-      </h1>
+
+      <BrowserRouter>
+        <Routes>
+
+          <Route path='/' element={<Login />} />
+
+
+          <Route path='/Register' element={<Register />} />
+          <Route path='/Home' element={<Home />} />
+          <Route path='/RegisterPaso2' element={<Register2 />} />
+          <Route path='/RegisterPaso3' element={<Register3 />} />
+          <Route path='/Transfers' element={<Transfer />} />
+          <Route path='/TransferStepContainer' element={<TransferStepContainer />} />
+          <Route path='/Pay' element={<Pay />} />
+          <Route path='/PayStepContainer' element={<PayStepContainer />} />
+          <Route path="*" element={<ErrorPage />} />
+
+
+        </Routes>
+      </BrowserRouter>
     </>
+
   )
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "#010101",
+        mediumGrey: "#B4B0B5",
+        lightGrey: "#E4E7F3",
+        darkGrey: "#575A5E",
+        greyDesign: "#D9D9D9",
+        primary: "#242054",
+        
+
+
+      },
+      // fontFamily: {
+      //   roboto: ['Roboto', 'sans-serif'],
+      // },
+    },
   },
   plugins: [],
 }
