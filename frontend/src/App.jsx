c5e93fefbbecb7ce71ab45cb46190969a5b6958d
GloriaRamosM <gloriaramosmoran@gmail.com>
2024-07-22 10:21:37 -0300

    cambio en el nombre de las carpeta pages, debe estar siempre en P mayuscula

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,23 @@
 
 import './App.css'
-import Login from "./pages/Login/Login.jsx"
-import Register from "./pages/register/Register.jsx"
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Login from "./Pages/Login/Login.jsx"
+import Register from "./Pages/register/Register.jsx"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Register2 from './pages/register/Register2.jsx';
-import Register3 from './pages/register/Register3.jsx';
-import Home from './pages/Home/Home.jsx'
-import Transfer from './pages/Transfer/Transfer'
+import Register2 from './Pages/register/Register2.jsx';
+import Register3 from './Pages/register/Register3.jsx';
+import Home from './Pages/Home/Home.jsx'
+import Transfer from './Pages/Transfer/Transfer.jsx'
 // import Footer from './components/Footer'
 // import Success from './Pages/Transfer/Success.jsx';
-import TransferStepContainer from './pages/Transfer/TransferStepContainer.jsx';
-import ErrorPage from './pages/ErrorPage/ErrorPage.jsx';
+import TransferStepContainer from './Pages/Transfer/TransferStepContainer.jsx';
+import ErrorPage from './Pages/ErrorPage/ErrorPage.jsx';
 
 import './css/main.css'
-import Pay from './pages/Servicespay/Pay.jsx';
-import PayStepContainer from './pages/Servicespay/PayStepContainer.jsx';
+import Pay from './Pages/Servicespay/Pay.jsx';
+import PayStepContainer from './Pages/Servicespay/PayStepContainer.jsx';
 
-import {AssistantProvider} from '../src/Providers/AssistantProvider.jsx'
+import { AssistantProvider } from '../src/Providers/AssistantProvider.jsx'
 
 import PrivateRoutes from './auth/PrivateRoutes.jsx';
 
@@ -29,22 +29,22 @@ function App() {
     <>
 
 
-    
+
       <AssistantProvider>
-      <BrowserRouter>
-      
-
-        <Routes>
-
-          <Route path='/' element={<Login />} />
+        <BrowserRouter>
 
 
-          <Route path='/Register' element={<Register />} />
-          <Route path='/RegisterPaso2' element={<Register2 />} />
-          <Route path='/RegisterPaso3' element={<Register3 />} />
+          <Routes>
 
-          
-            <Route element={<PrivateRoutes />} > 
+            <Route path='/' element={<Login />} />
+
+
+            <Route path='/Register' element={<Register />} />
+            <Route path='/RegisterPaso2' element={<Register2 />} />
+            <Route path='/RegisterPaso3' element={<Register3 />} />
+
+
+            <Route element={<PrivateRoutes />} >
               <Route path='/Home' element={<Home />} />
               <Route path='/Transfers' element={<Transfer />} />
               <Route path='/TransferStepContainer' element={<TransferStepContainer />} />
@@ -52,18 +52,18 @@ function App() {
               <Route path='/PayStepContainer' element={<PayStepContainer />} />
             </Route>
 
-            
-          
-          
-          <Route path="*" element={<ErrorPage />} />
 
 
-        </Routes>
 
-        
-      </BrowserRouter>
+            <Route path="*" element={<ErrorPage />} />
+
+
+          </Routes>
+
+
+        </BrowserRouter>
       </AssistantProvider>
-      
+
 
     </>
 
