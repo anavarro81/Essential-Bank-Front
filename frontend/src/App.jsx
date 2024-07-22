c4b544ff482080c8ddf1d3a9e3091068fc851065
anavarro81 <87661508+anavarro81@users.noreply.github.com>
2024-07-22 07:52:35 +0200

    Proteger rutas (#23)
    * feat: Progeger rutas
    
    Se protegen todas las rutas menos login y register para uqe si no está logado redirija a la pagina de login
    
    * fix: Se excluye registerPaso2 y registerPaso2 de las rutas protegidas
    
    Se excluye registerPaso2 y registerPaso2 de las rutas protegidas. Si no esta redirigiendo a Login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 import './App.css'
 import Login from "./pages/Login/Login.jsx"
 import Register from "./pages/register/Register.jsx"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 import Register2 from './pages/register/Register2.jsx';
 import Register3 from './pages/register/Register3.jsx';
@@ -19,7 +19,7 @@ import PayStepContainer from './pages/Servicespay/PayStepContainer.jsx';
 
 import {AssistantProvider} from '../src/Providers/AssistantProvider.jsx'
 
-
+import PrivateRoutes from './auth/PrivateRoutes.jsx';
 
 function App() {
 
@@ -40,13 +40,21 @@ function App() {
 
 
           <Route path='/Register' element={<Register />} />
-          <Route path='/Home' element={<Home />} />
           <Route path='/RegisterPaso2' element={<Register2 />} />
           <Route path='/RegisterPaso3' element={<Register3 />} />
-          <Route path='/Transfers' element={<Transfer />} />
-          <Route path='/TransferStepContainer' element={<TransferStepContainer />} />
-          <Route path='/Pay' element={<Pay />} />
-          <Route path='/PayStepContainer' element={<PayStepContainer />} />
+
+          
+            <Route element={<PrivateRoutes />} > 
+              <Route path='/Home' element={<Home />} />
+              <Route path='/Transfers' element={<Transfer />} />
+              <Route path='/TransferStepContainer' element={<TransferStepContainer />} />
+              <Route path='/Pay' element={<Pay />} />
+              <Route path='/PayStepContainer' element={<PayStepContainer />} />
+            </Route>
+
+            
+          
+          
           <Route path="*" element={<ErrorPage />} />
 
 
