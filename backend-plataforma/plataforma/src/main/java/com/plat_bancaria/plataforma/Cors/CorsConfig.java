9f4408ff26ef4c9e75ae4c4f76bcea09a180565b
An7h0ny1 <91570972+An7h0ny1@users.noreply.github.com>
2024-07-24 08:30:27 -0500

    ci: se configura la construccion automatica de la imagen de docker

--- backend-plataforma/plataforma/src/main/java/com/plat_bancaria/plataforma/Cors/CorsConfig.java
+++ backend-plataforma/plataforma/src/main/java/com/plat_bancaria/plataforma/Cors/CorsConfig.java
@@ -14,10 +14,11 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("*", "https://plataforma-i.onrender.com", "http://localhost:5173", "https://c19-16-m-java-react.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
+
             }
         };
     }
